77be6eb44796f6206a5587d6f27b1b14d04d78bd
dozzie89 <dylanosborn@mines.edu>
2025-02-04 14:36:31 -0700

    real first commit

---
+++ player.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+@export var health = 3
+@export var speed = 3
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$AnimatedSprite2D.play()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
